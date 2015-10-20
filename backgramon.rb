41ba7500bf61403932d6c2a4861e1172b7b87c2f
PropylBenzene <Nonas.Nonce@gmail.com>
2015-10-19 22:09:15 -0700

    Massive display upgrade.

--- backgramon.rb
+++ backgramon.rb
@@ -23,8 +23,8 @@ user_id = user_data[0]["id"] #Seperates out their userID for polling.
 recent_media = Instagram.user_recent_media(user_id)
 
 # Print out the result
-puts user_data.length
-puts user_data.inspect  
+#puts user_data.length
+#puts user_data.inspect  
 
 temp_post_text = recent_media.each.map {|x| x['caption']['text']}
 
@@ -32,18 +32,17 @@ temp_standard_image = recent_media.each.map {|x| x['images']['standard_resolutio
 
 temp_comments_poster = recent_media.each.map{|x| x['comments']['data'].each.map{|y| y['from']['username']}}
 
-
-
 temp_comments_made = recent_media.each.map{|x| x['comments']['data'].each.map {|y| y['text']}}
 
-puts "#{ARGV[0]}'s Instagram Feed Test"
-puts temp_post_text[0]
-puts temp_comments_made[0]
-puts temp_comments_poster[0]
+puts "\n\n#{ARGV[0].capitalize}'s Instagram Feed Backup\n\n"
 
-comments = temp_comments_poster.each.zip(temp_comments_made.each)
-puts comments
-# When dealing with all of this, you have to define the hash as you would an array point. aka [0]['caption']
-
-# a =  user_comments[0].caption.text
-
+@nothing = ""
+@total = ""
+for i in 0..(recent_media.length - 1) do
+	puts "TITLE:\t #{temp_post_text[i]} \n ------------------- \nIMAGE URL:\t #{temp_standard_image[i]} \n\n"
+	comment = temp_comments_poster[i].flatten.zip(temp_comments_made[i].flatten)
+	for k in 0..(comment.length - 1) do
+	puts comment[k].join("\t")
+end
+	puts "\n\n"	
+end
